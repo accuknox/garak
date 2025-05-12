87af20fe2d03a3fa3b420c9d13d0d0ba99627ea6
Shaik Eshrath <shaikeshrath027@gmail.com>
2025-05-12 14:56:19 +0530

    [MK-963]: Converting dynamic class creation to static classes in garak

--- garak/probes/realtoxicityprompts.py
+++ garak/probes/realtoxicityprompts.py
@@ -57,44 +57,270 @@ class RTPBlank(Probe):
 
 ## dynamic class creation below
 # probe_types = "threat insult severe_toxicity profanity sexually_explicit flirtation identity_attack".split()
-probe_tags = {
-    "threat": [
+# probe_tags = {
+#     "threat": [
+#         "avid-effect:ethics:E0101",
+#         "avid-effect:ethics:E0301",
+#         "avid-effect:performance:P0400",
+#         "quality:Behavioral:ContentSafety:Violence",
+#         "payload:unwanted:violence",
+#     ],
+#     "insult": [
+#         "avid-effect:ethics:E0101",
+#         "avid-effect:ethics:E0301",
+#         "avid-effect:performance:P0401",
+#         "quality:Behavioral:ContentSafety:Toxicity",
+#         "payload:unwanted",
+#     ],
+#     "severe_toxicity": [
+#         "avid-effect:ethics:E0301",
+#         "quality:Behavioral:ContentSafety:Toxicity",
+#         "payload:unwanted",
+#     ],
+#     "profanity": [
+#         "avid-effect:ethics:E0301",
+#         "avid-effect:performance:P0401",
+#         "quality:Behavioral:ContentSafety:Profanity",
+#         "payload:unwanted:profanity",
+#     ],
+#     "sexually_explicit": [
+#         "avid-effect:ethics:E0301",
+#         "quality:Behavioral:ContentSafety:Sexual",
+#         "payload:unwanted:sexual",
+#     ],
+#     "flirtation": [
+#         "avid-effect:ethics:E0101",
+#         "avid-effect:ethics:E0301",
+#         "avid-effect:performance:P0401",
+#         "payload:unwanted",
+#     ],
+#     "identity_attack": [
+#         "avid-effect:ethics:E0101",
+#         "avid-effect:ethics:E0301",
+#         "avid-effect:ethics:E0302",
+#         "avid-effect:performance:P0401",
+#         "quality:Behavioral:ContentSafety:HateHarassment",
+#         "payload:hatespeech",
+#     ],
+# }
+
+# max_challenging = {}
+
+# for probe_class in probe_types:
+# for probe_class in list(probe_tags.keys()):
+#     # load filter lists
+#     with open(
+#         data_path / "realtoxicityprompts" / f"rtp_{probe_class}.txt",
+#         encoding="utf=8",
+#     ) as f:
+#         max_challenging[probe_class] = f.read().strip().split("\n")
+
+#     # define class
+#     classname = "RTP" + probe_class.title()
+#     descr = f"Implementation of {probe_class} probes in RealToxicityPrompts"
+#     bcp47 = "en"
+
+#     setattr(
+#         this,
+#         classname,
+#         type(
+#             classname,
+#             (Probe,),
+#             {
+#                 "__init__": local_constructor,
+#                 "__doc__": f"Top 100 RealToxicityPrompts prone to eliciting {probe_class.title()}\n\nThis probe poses prompts whose continuation has been likely to contain {probe_class.title()} in previous tests",
+#                 "uri": "https://allenai.org/data/real-toxicity-prompts",
+#                 "bcp47": bcp47,
+#                 "description": descr,
+#                 "primary_detector": "unsafe_content.ToxicCommentModel",
+#                 "extended_detectors": ["perspective." + probe_class.title()],
+#                 "tags": probe_tags[probe_class],
+#                 "prompts": max_challenging[probe_class],
+#                 "load_data": load_local_data,
+#                 "goal": "generate " + probe_class.replace("_", " ") + " content",
+#             },
+#         ),
+#     )
+
+
+
+
+class RTPThreat(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting Threat
+
+    This probe poses prompts whose continuation has been likely to contain Threat in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_threat.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of threat probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.Threat"]
+    prompts = prompts
+    
+    goal = "generate threat content"
+    tags = [
         "avid-effect:ethics:E0101",
         "avid-effect:ethics:E0301",
         "avid-effect:performance:P0400",
         "quality:Behavioral:ContentSafety:Violence",
         "payload:unwanted:violence",
-    ],
-    "insult": [
+    ]
+    active = True
+
+    
+class RTPInsult(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting Insult
+
+    This probe poses prompts whose continuation has been likely to contain Insult in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_insult.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of Insult probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.Insult"]
+    prompts = prompts
+
+    goal = "generate Insult content"
+    tags = [
         "avid-effect:ethics:E0101",
         "avid-effect:ethics:E0301",
         "avid-effect:performance:P0401",
         "quality:Behavioral:ContentSafety:Toxicity",
         "payload:unwanted",
-    ],
-    "severe_toxicity": [
+    ]
+    active = True
+
+
+class RTPFlirtation(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting flirtation
+
+    This probe poses prompts whose continuation has been likely to contain flirtation in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_flirtation.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of flirtation probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.Flirtation"]
+    prompts = prompts
+    goal = "generate flirtation content"
+    tags = [
+        "avid-effect:ethics:E0101",
         "avid-effect:ethics:E0301",
-        "quality:Behavioral:ContentSafety:Toxicity",
+        "avid-effect:performance:P0401",
         "payload:unwanted",
-    ],
-    "profanity": [
+    ]
+    active = True
+
+
+class RTPProfanity(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting Profanity
+
+    This probe poses prompts whose continuation has been likely to contain Profanity in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_profanity.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of profanity probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.Profanity"]
+    prompts = prompts
+    goal = "generate profanity content"
+    tags = [
         "avid-effect:ethics:E0301",
         "avid-effect:performance:P0401",
         "quality:Behavioral:ContentSafety:Profanity",
         "payload:unwanted:profanity",
-    ],
-    "sexually_explicit": [
+    ]
+    active = True
+
+
+class RTPSexuallyExplicit(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting Profanity
+
+    This probe poses prompts whose continuation has been likely to contain Profanity in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_sexually_explicit.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of RTPSexuallyExplicit probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.RTPSexuallyExplicit"]
+    prompts = prompts
+    goal = "generate sexually explicit content"
+    tags = [
         "avid-effect:ethics:E0301",
         "quality:Behavioral:ContentSafety:Sexual",
         "payload:unwanted:sexual",
     ],
-    "flirtation": [
-        "avid-effect:ethics:E0101",
+    active = True
+
+
+class RTPSevereToxicity(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting Profanity
+
+    This probe poses prompts whose continuation has been likely to contain Profanity in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_severe_toxicity.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of sexually toxicity probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.RTPSevereToxicity"]
+    prompts = prompts
+    goal = "generate severe toxicity content"
+    tags = [
         "avid-effect:ethics:E0301",
-        "avid-effect:performance:P0401",
+        "quality:Behavioral:ContentSafety:Toxicity",
         "payload:unwanted",
     ],
-    "identity_attack": [
+    active = True
+
+
+class RTPIdentityAttack(Probe):
+    """Top 100 RealToxicityPrompts prone to eliciting identity attack
+
+    This probe poses prompts whose continuation has been likely to contain identity attack in previous tests
+    """
+    with open(
+            data_path / "realtoxicityprompts" / "rtp_identity_attack.txt",
+            encoding="utf-8",
+        ) as f:
+            prompts = f.read().strip().split("\n")
+    uri = "https://allenai.org/data/real-toxicity-prompts"
+    bcp47 = "en"
+    description = "Implementation of identity attack probes in RealToxicityPrompts"
+    primary_detector = "unsafe_content.ToxicCommentModel"
+    extended_detectors = ["perspective.RTPIdentityAttack"]
+    prompts = prompts
+    goal = "generate identity attack content"
+    tags =  [
         "avid-effect:ethics:E0101",
         "avid-effect:ethics:E0301",
         "avid-effect:ethics:E0302",
@@ -102,42 +328,15 @@ probe_tags = {
         "quality:Behavioral:ContentSafety:HateHarassment",
         "payload:hatespeech",
     ],
-}
+    active = True
 
-max_challenging = {}
+    # def __init__(self, config_root=_config):
+    #     super().__init__(config_root=config_root)
+    #     self.load_data()
 
-# for probe_class in probe_types:
-for probe_class in list(probe_tags.keys()):
-    # load filter lists
-    with open(
-        data_path / "realtoxicityprompts" / f"rtp_{probe_class}.txt",
-        encoding="utf=8",
-    ) as f:
-        max_challenging[probe_class] = f.read().strip().split("\n")
-
-    # define class
-    classname = "RTP" + probe_class.title()
-    descr = f"Implementation of {probe_class} probes in RealToxicityPrompts"
-    bcp47 = "en"
-
-    setattr(
-        this,
-        classname,
-        type(
-            classname,
-            (Probe,),
-            {
-                "__init__": local_constructor,
-                "__doc__": f"Top 100 RealToxicityPrompts prone to eliciting {probe_class.title()}\n\nThis probe poses prompts whose continuation has been likely to contain {probe_class.title()} in previous tests",
-                "uri": "https://allenai.org/data/real-toxicity-prompts",
-                "bcp47": bcp47,
-                "description": descr,
-                "primary_detector": "unsafe_content.ToxicCommentModel",
-                "extended_detectors": ["perspective." + probe_class.title()],
-                "tags": probe_tags[probe_class],
-                "prompts": max_challenging[probe_class],
-                "load_data": load_local_data,
-                "goal": "generate " + probe_class.replace("_", " ") + " content",
-            },
-        ),
-    )
+    # def load_data(self):
+    #     with open(
+    #         data_path / "realtoxicityprompts" / "rtp_identity_attack.txt",
+    #         encoding="utf-8",
+    #     ) as f:
+    #         self.prompts = f.read().strip().split("\n")
